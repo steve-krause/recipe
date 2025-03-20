4ad09adbde3d4b32917b604a03fee2407e252576
Steve Krause <sgkrause@gmail.com>
2025-03-20 18:47:21 -0400

    first, basic page layout

---
+++ src/Data.tsx
@@ -0,0 +1,88 @@
+const Data = {
+  "Recipes": [{
+    "Title": "Apple Banana Quinoa Breakfast Cups",
+    "Description": null,
+    "Source": null,
+    "Original URL": null,
+    "Yield": null,
+    "Active": null,
+    "Total": null,
+    "Ingredients": [
+      "1/2 cup applesauce",
+      "1 cup mashed banana (about 3 bananas)",
+      "1 banana for slicing",
+      "1 cup cooked quinoa (about 1/2 cup dry)",
+      "2 1/2 cups old-fashioned oats",
+      "1/2 cup almond milk",
+      "1/4 cup honey",
+      "1 tsp vanilla extract",
+      "1 tsp cinnamon",
+      "1 apple, peeled and chopped"
+    ],
+    "Instructions": [
+      "Preheat oven to 375 degrees. Lightly grease a muffin tin (I use olive oil).",
+      "Cook the quinoa. Bring 3/4 up water to a boil, pour in 1/2 cup dry quinoa, reduce to a simmer until fluffy-about 12 minutes.",
+      "Mix applesauce, mashed banana, almond milk, honey and vanilla in a bowl.",
+      "Mix dry ingredients (quinoa, oats, cinnamon) in a separate bowl. Slowly stir the wet into the dry until fully combined.",
+      "Peel core and chop up an apple. Mix the apple chunks into the bowl.",
+      "Fill each of the muffin cups to the top with the quinoa mixture. Add a banana slice or two to the top of each.",
+      "Bake for 20-25 minutes.",
+      "Let cool for 5 minutes, then enjoy one warm!"
+    ],
+    "Notes": "Store in an airtight container in the fridge. Reheat leftovers in the microwave before eating. You can also add some crushed pecans or walnuts to these--experiment!"
+  },
+  {
+    "Title": "Asian Cabbage Mango Slaw",
+    "Description": null,
+    "Source": "Skinnytaste",
+    "Original URL": "http://www.skinnytaste.com/2012/04/asian-cabbage-mango-slaw.html",
+    "Yield": 6,
+    "Active": null,
+    "Total": null,
+    "Image": "https://cdn2.pepperplate.com/recipes/a2074c5760274744b54b5a98da8286e2.jpg",
+    "Ingredients": [
+      "2 cups shredded cabbage",
+      "1/2 cup shredded carrots",
+      "1 mango, not fully ripe",
+      "3 medium scallions, chopped",
+      "3 tbsp rice vinegar",
+      "1/2 lime, juiced",
+      "1 tbsp low sodium soy sauce (or tamari for gluten free)",
+      "1 tbsp sesame oil",
+      "1 tsp black and white sesame seeds"
+    ],
+    "Instructions": [
+      "Julienne the mango (cut into strips). Combine with the cabbage, carrots, scallions and toss together in a large bowl.",
+      "Prepare the dressing by whisking together the rice vinegar, soy sauce, and lime juice; slowly whisk in the oil.",
+      "Pour the dressing over the cabbage mixture and toss to coat. Let it sit at least 15-20 minutes to let the dressing soak in.",
+      "Serve sprinkled with sesame seeds.",
+      "Makes approximately 3 cups. Double it for more servings."
+    ]
+  },
+  {
+    "Title": "Best Grilled Pork Chops",
+    "Description": null,
+    "Source": "Allrecipes.com",
+    "Original URL": "http://allrecipes.com/recipe/223405/best-grilled-pork-chops/",
+    "Yield": "6 servings",
+    "Active": "5 minutes",
+    "Total": "2 hours 15 minutes",
+    "Image": "https://cdn2.pepperplate.com/recipes/50e981aa9c0940a5b2c16863508541ff.jpg",
+    "Ingredients": [
+      "1/2 cup water",
+      "1/3 cup light soy sauce",
+      "1/4 cup vegetable oil",
+      "3 tablespoons lemon pepper seasoning",
+      "2 teaspoons minced garlic",
+      "6 boneless pork loin chops, trimmed of fat"
+    ],
+    "Instructions": [
+      "Mix water, soy sauce, vegetable oil, lemon pepper seasoning, and minced garlic in a deep bowl; add pork chops and marinate in refrigerator at least 2 hours.",
+      "Preheat an outdoor grill for medium-high heat and lightly oil the grate.",
+      "Remove pork chops from the marinade and shake off excess. Discard the remaining marinade.",
+      "Cook the pork chops on the preheated grill until no longer pink in the center, 5 to 6 minutes per side. An instant-read thermometer inserted into the center should read 145 degrees F (63 degrees C)."
+    ]
+  }]
+}
+
+export default Data;
